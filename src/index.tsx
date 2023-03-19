986bcef7c99f4a1ad3a4fe1ddf38e258343b4918
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2023-03-19 14:18:35 -0400

    handle cases for more kinds of form elements
    - disabling elements ignores them for form data
    - checkboxes and radio elements are now properly handled
    - elements with children (i.e.) selects, fieldsets, etc, now look into the children

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,67 @@ interface Props<T> {
   schema: { [Key in keyof T]: (value: T[Key]) => boolean }
 }
 
+function as<T extends Element>(element: Element) {
+  return element as T
+}
+
+function addFormData<T>(formElement: Element, formData: T) {
+  // @ts-ignore
+  if (formElement.disabled) return
+
+  switch (formElement.tagName.toLowerCase()) {
+    case 'input':
+      const inputEl = as<HTMLInputElement>(formElement)
+
+      if (inputEl.type === 'radio') {
+        if (inputEl.checked) {
+          formData[inputEl.name as keyof T] = inputEl.value as T[keyof T]
+        }
+      } else if (inputEl.type === 'checkbox') {
+        if (inputEl.checked) {
+          // @ts-ignore
+          formData[inputEl.name as keyof T] = formData.hasOwnProperty(
+            inputEl.name
+          )
+            ? [
+                ...(formData[inputEl.name as keyof T] as string[]),
+                inputEl.value
+              ]
+            : [inputEl.value]
+        }
+      } else {
+        formData[inputEl.name as keyof T] = inputEl.value as T[keyof T]
+      }
+      break
+    case 'select':
+      const selectEl = as<HTMLSelectElement>(formElement)
+
+      formData[selectEl.name as keyof T] = Array.from(
+        selectEl.selectedOptions
+      ).map((option) => option.value) as T[keyof T]
+      break
+    case 'textarea':
+      const textAreaEl = as<HTMLTextAreaElement>(formElement)
+
+      formData[textAreaEl.name as keyof T] = textAreaEl.value as T[keyof T]
+      break
+    case 'output':
+      const outputEl = as<HTMLOutputElement>(formElement)
+
+      formData[outputEl.name as keyof T] = outputEl.value as T[keyof T]
+      break
+    case 'fieldset':
+      const fieldSetEl = as<HTMLFieldSetElement>(formElement)
+
+      for (const fieldSetChild of Array.from(fieldSetEl.children)) {
+        addFormData(fieldSetChild, formData)
+      }
+      break
+    default:
+      break
+  }
+}
+
 export function useForm<TFormData>({ schema }: Props<TFormData>) {
   const [errors, setErrors] = useState({
     ...Object.fromEntries(
@@ -24,28 +85,32 @@ export function useForm<TFormData>({ schema }: Props<TFormData>) {
       e.preventDefault()
 
       const formData = {} as TFormData
-      const formElements = Array.from(e.currentTarget.elements) as unknown as {
-        name: keyof TFormData
-        value: TFormData[keyof TFormData]
-      }[]
-
       const errorsObject = {} as { [Key in keyof TFormData]: boolean }
 
+      const formElements = Array.from(e.currentTarget.elements).filter((el) =>
+        new Set(['input', 'select', 'textarea', 'output', 'fieldset']).has(
+          el.tagName.toLowerCase()
+        )
+      )
+
       for (const formElement of formElements) {
-        if (formElement.name) {
-          formData[formElement.name] = formElement.value
+        // @ts-ignore
+        if (!formElement.name) continue
+        addFormData(formElement, formData)
 
-          const isError = schema.hasOwnProperty(formElement.name)
-            ? !schema[formElement.name](formElement.value)
-            : false
+        // @ts-ignore
+        const isError = schema.hasOwnProperty(formElement.name)
+          ? // @ts-ignore
+            !schema[formElement.name](formData[formElement.name])
+          : false
 
-          errorsObject[formElement.name] = isError
-        }
+        // @ts-ignore
+        errorsObject[formElement.name] = isError
       }
 
       setErrors(errorsObject)
 
-      if (Object.values(errorsObject).every((value) => value === false)) {
+      if (Object.values(errorsObject).every((isError) => isError === false)) {
         handleSubmit({ e, formData })
       }
     }
